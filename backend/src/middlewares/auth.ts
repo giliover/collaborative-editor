a0f592a86295058591facec5037d65d63df23d88
giliover <giliover@hotmail.com>
2024-10-08 11:24:30 -0300

    chore(backend): add middleware

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface AuthRequest extends Request {
+  user?: string;
+}
+
+export const authenticateToken = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+): void => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) {
+    res.sendStatus(401);
+    return;
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET || "secret", (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = (user as any).id;
+    next();
+  });
+};
