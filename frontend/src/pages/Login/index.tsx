8c49243eb6665d68bf92af04a6b510793b6c67fc
giliover <giliover@hotmail.com>
2024-10-08 12:35:46 -0300

    fix(pages): add validator & create initial document

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -9,20 +9,42 @@ import {
   LoginForm,
   LoginTitle,
 } from "./styles";
+import UserValidator from "../../validators/UserValidator";
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
+  const createInitialDocument = async (error: string) => {
+    try {
+      if (error.includes("Erro ao obter documento")) {
+        const docRes = await api.post("/documents", {
+          title: "Novo documento",
+        });
+        const documentId = docRes.data._id;
+
+        navigate(`/documents/${documentId}`);
+        return;
+      }
+    } catch (error: any) {
+      console.error(error.response.data.error);
+    }
+  };
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await api.post("/auth/login", { email, password });
+      const payload = { email, password };
+      const payloadValid = await UserValidator.validate(payload);
+      const res = await api.post("/auth/login", payloadValid);
       localStorage.setItem("token", res.data.token);
-      navigate("/documents/1");
-    } catch (error) {
-      console.error("Erro ao fazer login", error);
+      api.defaults.headers.common["Authorization"] = `Bearer ${res.data.token}`;
+      const docRes = await api.get("/documents/my-document");
+      const documentId = docRes.data._id;
+      navigate(`/documents/${documentId}`);
+    } catch (error: any) {
+      await createInitialDocument(error?.response?.data?.error);
     }
   };
 
