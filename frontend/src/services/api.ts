8e099873a83b1e2851619d1e7c88f32b267538e7
giliover <giliover@hotmail.com>
2024-10-09 09:29:21 -0300

    chore(services): add interceptor to api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,20 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+import { getUserIdAndToken } from "../global/getUserIdAndToken";
+
+const authorizationInterceptor: any = async (config: AxiosRequestConfig) => {
+  if (!config.headers?.Authorization) {
+    const { token } = getUserIdAndToken();
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+};
 
 const api = axios.create({
   baseURL: "http://localhost:5000/api",
 });
+
+api.interceptors.request.use(authorizationInterceptor);
 
 export default api;
